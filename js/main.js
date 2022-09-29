f00012724a277b028832d35983be3a01f4e61aa3
Andrés Mauricio Martínez Hincapié - Ceiba Software <andres.hincapie@ceiba.com.co>
2022-09-29 18:05:35 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function changeColor(newColor) {
+    const elem = document.getElementById('para');
+    elem.style.color = newColor;
+  }
+  
+let firstHeading = document.querySelector('h1');
+
+console.log(firstHeading);
